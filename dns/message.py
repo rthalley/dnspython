b1ac611ba847e9df17cb46150e04d5b2699a72b7
Bob Halley <halley@dnspython.org>
2005-09-02 05:32:52 +0000

    update copyrights
    Original author: Bob Halley <halley@dnspython.org>
    Date: 2005-01-08 08:13:48

--- dns/message.py
+++ dns/message.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2004 Nominum, Inc.
+# Copyright (C) 2001-2005 Nominum, Inc.
 #
 # Permission to use, copy, modify, and distribute this software and its
 # documentation for any purpose with or without fee is hereby granted,
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2003, 2004 Nominum, Inc.
+# Copyright (C) 2003-2005 Nominum, Inc.
 #
 # Permission to use, copy, modify, and distribute this software and its
 # documentation for any purpose with or without fee is hereby granted,
