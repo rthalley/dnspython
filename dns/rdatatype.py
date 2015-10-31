084516e17261a21cea4871d0b5a8bc544f6beb8d
Jan Vcelak <jv@fcelda.cz>
2015-10-31 12:19:06 +0100

    Add support for CDNSKEY record type

--- dns/rdatatype.py
+++ dns/rdatatype.py
@@ -81,6 +81,7 @@ NSEC3PARAM = 51
 TLSA = 52
 HIP = 55
 CDS = 59
+CDNSKEY = 60
 SPF = 99
 UNSPEC = 103
 TKEY = 249
@@ -147,6 +148,7 @@ _by_text = {
     'TLSA' : TLSA,
     'HIP' : HIP,
     'CDS' : CDS,
+    'CDNSKEY' : CDNSKEY,
     'SPF' : SPF,
     'UNSPEC' : UNSPEC,
     'TKEY' : TKEY,
