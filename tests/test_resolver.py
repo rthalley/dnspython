6b6117d6bd822cf8b8dd313687c8534e6fbc7dbb
Bob Halley <halley@dnspython.org>
2017-01-09 06:12:43 -0800

    bump copyright

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2003-2007, 2009-2011 Nominum, Inc.
+# Copyright (C) 2003-2017 Nominum, Inc.
 #
 # Permission to use, copy, modify, and distribute this software and its
 # documentation for any purpose with or without fee is hereby granted,
