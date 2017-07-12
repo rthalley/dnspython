ccfa25d25e21a978d5be5dcc00e5d7b25e7639be
Markus Lange <markus@egn.al>
2017-07-12 17:14:48 +0200

    Extend zone.to_{file,text}, node.to_text and rdataset.to_text function to be able to output prettier b introducing an user defined printf format string as parameter.
     Please enter the commit message for your changes. Lines starting

--- dns/node.py
+++ dns/node.py
@@ -32,13 +32,20 @@ class Node(object):
         #: the set of rdatsets, represented as a list.
         self.rdatasets = []
 
-    def to_text(self, name, want_comment=False, **kw):
+    def to_text(self, name,
+                pprint=u'%(name)s%(ttl)d %(rdclass)s %(rdtype)s %(rdata)s%(rcomment)s\n',
+                want_comment=False, **kw):
         """Convert a node to text format.
 
         Each rdataset at the node is printed.  Any keyword arguments
         to this method are passed on to the rdataset's to_text() method.
 
         *name*, a ``dns.name.Name``, the owner name of the rdatasets.
+        
+        *pprint*, a ``string``. A printf format string for pretty printing
+        the output. The following mapping keys need to be used:
+        ``name``, ``ttl``, ``rdclass``, ``rdtype``, ``rdata`` and ``rcomment``.
+        
         *want_comment*, a ``bool`` if True, stored comments will be printed 
         behind the records.
 
@@ -48,7 +55,8 @@ class Node(object):
         s = StringIO()
         for rds in self.rdatasets:
             if len(rds) > 0:
-                s.write(rds.to_text(name, want_comment=want_comment, **kw))
+                s.write(rds.to_text(name, pprint=pprint,
+                        want_comment=want_comment, **kw))
                 s.write(u'\n')
         return s.getvalue()[:-1]
 
