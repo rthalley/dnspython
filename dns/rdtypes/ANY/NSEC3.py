2844d249dc4709e8c7b9e9a2fc1a503548e1515b
Martin <martin.basti@gmail.com>
2017-07-14 20:36:54 +0200

    py3: maketrans: use rather if statement
    If statement is faster and it makes clear intention that difference is
    due PY2 and PY3

--- dns/rdtypes/ANY/NSEC3.py
+++ dns/rdtypes/ANY/NSEC3.py
@@ -17,6 +17,7 @@ import base64
 import binascii
 import string
 import struct
+import sys
 
 import dns.exception
 import dns.rdata
@@ -24,17 +25,18 @@ import dns.rdatatype
 from dns._compat import xrange, text_type
 
 # pylint: disable=deprecated-string-function
-try:
-    b32_hex_to_normal = string.maketrans('0123456789ABCDEFGHIJKLMNOPQRSTUV',
-                                         'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567')
-    b32_normal_to_hex = string.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
-                                         '0123456789ABCDEFGHIJKLMNOPQRSTUV')
-except AttributeError:
+if sys.version_info > (3,):
     b32_hex_to_normal = bytes.maketrans(b'0123456789ABCDEFGHIJKLMNOPQRSTUV',
                                         b'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567')
     b32_normal_to_hex = bytes.maketrans(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
                                         b'0123456789ABCDEFGHIJKLMNOPQRSTUV')
+else:
+    b32_hex_to_normal = string.maketrans('0123456789ABCDEFGHIJKLMNOPQRSTUV',
+                                         'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567')
+    b32_normal_to_hex = string.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
+                                         '0123456789ABCDEFGHIJKLMNOPQRSTUV')
 # pylint: enable=deprecated-string-function
+
 
 # hash algorithm constants
 SHA1 = 1
