981722b41c1fe9059544a36b5deb14f00536da65
Bob Halley <halley@dnspython.org>
2020-06-05 09:46:31 -0700

    remove no longer needed backwards compat for collections.abc.MutableMapping

--- dns/namedict.py
+++ dns/namedict.py
@@ -27,10 +27,8 @@
 
 """DNS name dictionary"""
 
-try:
-    from collections.abc import MutableMapping
-except ImportError:
-    from collections import MutableMapping
+from collections.abc import MutableMapping
+
 import dns.name
 
 
