c8ec6ca47397420cb128db9aa4951245ac4ef07f
Brian Wellington <bwelling@xbill.org>
2020-06-30 10:46:24 -0700

    Update for OPT/TSIG changes.

--- dns/message.pyi
+++ dns/message.pyi
@@ -16,24 +16,14 @@ class Message:
         self.answer : List[rrset.RRset] = []
         self.authority : List[rrset.RRset] = []
         self.additional : List[rrset.RRset] = []
-        self.edns = -1
-        self.ednsflags = 0
-        self.payload = 0
-        self.options : List[edns.Option] = []
+        self.opt : rrset.RRset = None
         self.request_payload = 0
         self.keyring = None
-        self.keyname = None
-        self.keyalgorithm = tsig.default_algorithm
+        self.tsig : rrset.RRset = None
         self.request_mac = b''
-        self.other_data = b''
-        self.tsig_error = 0
-        self.fudge = 300
-        self.original_id = self.id
-        self.mac = b''
         self.xfr = False
         self.origin = None
         self.tsig_ctx = None
-        self.had_tsig = False
         self.index : Dict[Tuple[rrset.RRset, name.Name, int, int, Union[int,str], int], rrset.RRset] = {}
 
     def is_response(self, other : Message) -> bool:
