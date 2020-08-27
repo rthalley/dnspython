638ff1b0c2247cd050076bec0a250060b3747b97
Bob Halley <halley@dnspython.org>
2020-08-27 08:26:04 -0700

    lint

--- dns/rdtypes/ANY/SMIMEA.py
+++ dns/rdtypes/ANY/SMIMEA.py
@@ -4,4 +4,5 @@ from dns.rdtypes.ANY.TLSA import TLSA
 
 @dns.immutable.immutable
 class SMIMEA(TLSA):
-    """SMIMEA record, same format as TLSA per https://tools.ietf.org/html/rfc8162#section-2"""
+    """SMIMEA record, same format as TLSA per
+    https://tools.ietf.org/html/rfc8162#section-2"""
