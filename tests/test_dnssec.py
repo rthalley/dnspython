3b7ede35cffc02f38ff537a0f239970f021bd492
Jakob Schlyter <jakob@kirei.se>
2022-12-05 20:37:21 +0100

    reformat

--- tests/test_dnssec.py
+++ tests/test_dnssec.py
@@ -823,9 +823,11 @@ class DNSSECMiscTestCase(unittest.TestCase):
     def testUnknownHash(self):
         with self.assertRaises(dns.dnssec.ValidationFailure):
             dns.dnssec._make_hash(100)
-    
+
     def testToTimestamp(self):
-        d1 = dns.dnssec.to_timestamp(datetime(year=1984, month=1, day=1, hour=13, minute=37))
+        d1 = dns.dnssec.to_timestamp(
+            datetime(year=1984, month=1, day=1, hour=13, minute=37)
+        )
         d2 = dns.dnssec.to_timestamp("19840101133700")
         d3 = dns.dnssec.to_timestamp(441808620.0)
         d4 = dns.dnssec.to_timestamp(441808620)
