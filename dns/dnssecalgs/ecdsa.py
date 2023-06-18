ad4a44f29f49542c3dfd0b23a1c4f68aa2585c60
Jakob Schlyter <jakob@kirei.se>
2023-06-18 10:45:44 +0200

    cleanup

--- dns/dnssecalgs/ecdsa.py
+++ dns/dnssecalgs/ecdsa.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec, utils
--- dns/dnssecalgs/eddsa.py
+++ dns/dnssecalgs/eddsa.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Type
 
 from cryptography.hazmat.primitives import serialization
