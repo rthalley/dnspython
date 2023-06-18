ed922c7dcda9e26aa46f7da1dd455a104214e26e
Jakob Schlyter <jakob@kirei.se>
2023-06-18 19:48:43 +0200

    docs

--- dns/dnssecalgs/base.py
+++ dns/dnssecalgs/base.py
@@ -14,13 +14,20 @@ class GenericPublicKey(ABC):
 
     @abstractmethod
     def verify(self, signature: bytes, data: bytes) -> None:
+        """Verify signed DNSSEC data"""
         pass
 
     @abstractmethod
     def encode_key_bytes(self) -> bytes:
         pass
 
+    @classmethod
+    def _ensure_algorithm_key_combination(cls, key: DNSKEY) -> None:
+        if key.algorithm != cls.algorithm:
+            raise AlgorithmKeyMismatch
+
     def to_dnskey(self, flags: int = Flag.ZONE, protocol: int = 3) -> DNSKEY:
+        """Return public key as DNSKEY"""
         return DNSKEY(
             rdclass=dns.rdataclass.IN,
             rdtype=dns.rdatatype.DNSKEY,
@@ -31,22 +38,20 @@ class GenericPublicKey(ABC):
         )
 
     @classmethod
-    def _ensure_algorithm_key_combination(cls, key: DNSKEY) -> None:
-        if key.algorithm != cls.algorithm:
-            raise AlgorithmKeyMismatch
-
-    @classmethod
     @abstractmethod
     def from_dnskey(cls, key: DNSKEY) -> "GenericPublicKey":
+        """Create public key from DNSKEY"""
         pass
 
     @classmethod
     @abstractmethod
     def from_pem(cls, public_pem: bytes) -> "GenericPublicKey":
+        """Create public key from PEM-encoded SubjectPublicKeyInfo as specified in RFC 5280"""
         pass
 
     @abstractmethod
     def to_pem(self) -> bytes:
+        """Return public-key as PEM-encoded SubjectPublicKeyInfo as specified in RFC 5280"""
         pass
 
 
@@ -55,10 +60,12 @@ class GenericPrivateKey(ABC):
 
     @abstractmethod
     def sign(self, data: bytes, verify: bool = False) -> bytes:
+        """Sign DNSSEC data"""
         pass
 
     @abstractmethod
     def public_key(self) -> "GenericPublicKey":
+        """Return public key instance"""
         pass
 
     @classmethod
@@ -66,8 +73,10 @@ class GenericPrivateKey(ABC):
     def from_pem(
         cls, private_pem: bytes, password: Optional[bytes] = None
     ) -> "GenericPrivateKey":
+        """Create private key from PEM-encoded PKCS#8"""
         pass
 
     @abstractmethod
     def to_pem(self, password: Optional[bytes] = None) -> bytes:
+        """Return private key as PEM-encoded PKCS#8"""
         pass
