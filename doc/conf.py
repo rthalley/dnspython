9acf6affbe1b852536544a8b5c51038fb371911b
Bob Halley <halley@dnspython.org>
2024-01-15 13:16:16 -0800

    bump readthedocs-sphinx-search version to 0.3.2

--- doc/conf.py
+++ doc/conf.py
@@ -70,7 +70,7 @@ release = "2.4.2"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -93,6 +93,7 @@ autoclass_content = "both"
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
