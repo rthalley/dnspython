21cda62e32e99b9fa2f39bd0e8a59a830245935d
Bob Halley <halley@dnspython.org>
2024-03-11 08:32:12 -0700

    Add a simple tsig example.

---
+++ examples/tsig.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import dns.message
+import dns.query
+import dns.tsig
+
+
+key = dns.tsig.Key("keyname.", "bnp6+y85UcBfsieuB/Uhx3EUsjc8wAFyyCSS5rhScb0=",
+                   algorithm=dns.tsig.HMAC_SHA256)
+
+
+q = dns.message.make_query("example.", "SOA")
+q.use_tsig(keyring=key)
+r = dns.query.udp(q, "127.0.0.1")   # your authority address here
+soa = r.find_rrset(r.answer, "example", "IN", "SOA")
+print(soa)
+
+
