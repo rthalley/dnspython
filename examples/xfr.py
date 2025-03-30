ac9e39551099b238245245565c556ddcc155b937
Bob Halley <halley@dnspython.org>
2025-03-30 12:24:37 -0700

    add comment that running xfr example does not work for access control reasons

--- examples/xfr.py
+++ examples/xfr.py
@@ -4,6 +4,9 @@ import dns.query
 import dns.resolver
 import dns.zone
 
+# Note that running this doesn't currently work because
+# dnspython.org's nameservers do not permit AXFR
+
 soa_answer = dns.resolver.resolve("dnspython.org", "SOA")
 master_answer = dns.resolver.resolve(soa_answer[0].mname, "A")
 
