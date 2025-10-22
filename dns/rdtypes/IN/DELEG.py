811eea5232975421925962e867d13b79d409bb29
Bob Halley <halley@dnspython.org>
2025-10-22 11:39:49 -0700

    DELEG draft support

---
+++ dns/rdtypes/IN/DELEG.py
@@ -0,0 +1,9 @@
+# Copyright (C) Dnspython Contributors, see LICENSE for text of ISC license
+
+import dns.immutable
+import dns.rdtypes.delegbase
+
+
+@dns.immutable.immutable  # pyright: ignore
+class DELEG(dns.rdtypes.delegbase.DelegBase):
+    """DELEG record"""
